7461ac6582cb26f9ffaea11912906e18e7661937
tangdi <tangdi0802@foxmail.com>
2018-02-02 14:31:57 +0800

    编辑框修改

--- nfc/js/pages/home/TabMainScreen.js
+++ nfc/js/pages/home/TabMainScreen.js
@@ -9,11 +9,18 @@ import {
     Animated,
     Image,
     Easing,
+    ScrollView
 } from 'react-native';
 
 let windowWidth = Dimensions.get('window').width;
 
 const DEVICE_WIDTH = Dimensions.get('window').width;
+
+import ModalDropdown from 'react-native-modal-dropdown';
+
+import { Kaede } from 'react-native-textinput-effects';
+
+import { KeyboardAwareScrollView } from 'react-native-keyboard-aware-scroll-view'
 
 const MARGIN = 40;
 
@@ -33,8 +40,16 @@ export default class App extends Component<{}> {
         this.state={
             idCard:'',
             carCard:'',
-            search:0,
+            searchType:'身份证',
         }
+    }
+
+    /**
+     * 点击按钮进入请求
+     * @private
+     */
+    _onPress(){
+
     }
 
 
@@ -42,15 +57,16 @@ export default class App extends Component<{}> {
 
     render() {
         return (
+            <KeyboardAwareScrollView>
             <View style={styles.container}>
 
                 <Madoka
                     style={[styles.inputStyle]}
                     label={'身份证'}
                     // this is used as active and passive border color
-                    borderColor={'#F035E0'}
+                    borderColor={'black'}
                     onChangeText={(text) => this.setState({idCard:text})}
-                    labelStyle={{ color: '#F035E0' }}
+                    labelStyle={{ color: 'black' }}
                     inputStyle={{ color: '#f4a197' }}
                 />
 
@@ -58,19 +74,32 @@ export default class App extends Component<{}> {
                     style={[styles.inputStyle]}
                     label={'车牌号'}
                     // this is used as active and passive border color
-                    borderColor={'#F035E0'}
+                    borderColor={'black'}
                     onChangeText={(text) => this.setState({carCard:text})}
-                    labelStyle={{ color: '#F035E0' }}
+                    labelStyle={{ color: 'black' }}
                     inputStyle={{ color: '#f4a197' }}
                 />
 
-                <TouchableOpacity style={styles.button4}>
-                    <Text>
-                        查询
+                <Kaede
+                    style={[styles.inputStyle]}
+                    label={'身份证'}
+                    labelStyle={{backgroundColor:'black'}}
+                />
+
+                <Kaede
+                    style={[styles.inputStyle]}
+                    label={'车牌号'}
+                />
+
+
+                <TouchableOpacity style={styles.button4} onPress={this._onPress()}>
+                    <Text style={{fontSize:16}}>
+                        查    询
                     </Text>
 
                 </TouchableOpacity>
             </View>
+            </KeyboardAwareScrollView>
         );
     }
 }
@@ -80,7 +109,9 @@ const styles = StyleSheet.create({
         flex: 1,
         justifyContent: 'center',
         alignItems: 'center',
-        backgroundColor: '#F5FCFF',
+        backgroundColor: '#4474BB',
+        width: Dimensions.get('window').width,
+        height: Dimensions.get('window').height,
     },
     welcome: {
         fontSize: 20,
@@ -93,7 +124,8 @@ const styles = StyleSheet.create({
         marginBottom: 5,
     },
     inputStyle:{
-        width:windowWidth,
+        width:windowWidth - 10,
+        marginBottom:15,
     },
 
     button4: {
@@ -101,7 +133,7 @@ const styles = StyleSheet.create({
         alignItems: 'center',
         justifyContent: 'center',
         backgroundColor: '#F035E0',
-        height: MARGIN,
+        height: 50,
         borderRadius: 20,
     },
 
@@ -116,6 +148,18 @@ const styles = StyleSheet.create({
     image4: {
         width: 24,
         height: 24,
+    },
+
+    selecttype:{
+        width:80,
+        height:40,
+        backgroundColor:'black'
+    },
+
+    imgStyle:{
+      width:80,
+      height:80,
+        marginBottom:10
     },
 
 });
