c5657b614ea2ef82c8e7e329cf19dbd7924c036b
tangdi <tangdi0802@foxmail.com>
2018-03-08 16:44:55 +0800

    界面调整

--- nfc/js/pages/home/nfchomepage.js
+++ nfc/js/pages/home/nfchomepage.js
@@ -80,8 +80,8 @@ export default class nfchomepage extends Component{
                 width:windowWidth, flexDirection:'row',
                 alignItems:'center',
                 backgroundColor:'#2554A2',
-                paddingTop:ScreenUtil.scaleSize(30),
-                paddingBottom:ScreenUtil.scaleSize(30)}}>
+                paddingTop:ScreenUtil.scaleSize(50),
+                paddingBottom:ScreenUtil.scaleSize(50)}}>
                 <View style={{flex:1, flexDirection:'row', marginLeft:ScreenUtil.scaleSize(20), alignItems:'center',}}>
                     <Image source={this.state.user_photo == ''? require('../../img/Mine/avatar.png'):{uri:this.state.user_photo}}
                            style={{width:ScreenUtil.scaleSize(44), height:ScreenUtil.scaleSize(44), borderRadius:ScreenUtil.scaleSize(22)}}/>
@@ -116,7 +116,7 @@ export default class nfchomepage extends Component{
                 </TextInput>
 
                 <TouchableOpacity onPress={()=>{this.props.navigation.navigate('SearchList')}} >
-                    <Image style={{width:ScreenUtil.scaleSize(110), height:ScreenUtil.scaleSize(88), padding:0}} source={require('../../nfcimg/search.png')}/>
+                    <Image style={{width:ScreenUtil.scaleSize(109), height:ScreenUtil.scaleSize(88), padding:0}} source={require('../../nfcimg/search.png')}/>
                 </TouchableOpacity>
 
             </View>
